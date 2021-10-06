3d82250b1e822c3eae98ac2a3f1d74e729124435
KonTagowski <konrad.tagowski97@gmail.com>
2021-10-06 11:22:20 +0200

    Small fix and upgrade: -Upgraded menu bar... -Small changes

---
+++ js/menubar-script.js
@@ -0,0 +1,24 @@
+
+function openMenu() {
+    var link = document.createElement('link');
+    link.href = "css/" + "Style-Menu.css";
+    console.log(document.styleSheets[1].href)
+    document.getElementById("main").style.transition = "margin-left .5s";
+    document.getElementById("header").style.transition = "margin-left .5s";
+    if (document.styleSheets[1].href == link.href) {
+        document.getElementById("myside-menu").style.width = "165px";
+        document.getElementById("main").style.marginLeft = "165px";
+        document.getElementById("header").style.marginLeft = "165px";
+    }
+    else {
+        document.getElementById("myside-menu").style.width = "150px";
+        document.getElementById("main").style.marginLeft = "0px";
+        document.getElementById("header").style.marginLeft = "0px";
+    }
+}
+
+function closeMenu() {
+    document.getElementById("myside-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("header").style.marginLeft = "0";
+}
--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
 var actual_StyleSheet = "StyleSheet.css";
 
-function changeStyle(href) {
-    for (var i = 0; i < document.styleSheets.length; i++) {
-        if (document.styleSheets[i].title == "Style") {
-            document.getElementsByTagName('head')[0].removeChild(document.styleSheets[i].ownerNode);
-        }
-    }
+function changeStyle(href, id_StyleSheet) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
     link.href = "css/" + href;
-    link.title = "Style";
-    document.getElementsByTagName('head')[0].appendChild(link)
-}
+    document.getElementsByTagName('head')[0].replaceChild(link, document.styleSheets[id_StyleSheet].ownerNode);
 
-function openMenu() {
-    document.getElementById("myside-menu").style.width = "150px";
-    document.getElementById("main").style.marginLeft = "150px";
-    console.log("Test - OpenMenu")
-  }
-  
-  function closeNav() {
-    document.getElementById("myside-menu").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+    //  for (var i = 0; i < document.styleSheets.length; i++) {
+    //      if (document.styleSheets[i].title == id_title) {
+    //          //document.getElementsByTagName('head')[0].removeChild(document.styleSheets[i].ownerNode);
+    //          document.getElementsByTagName('head')[0].replaceChild(link, document.styleSheets[i].ownerNode);
+    //      }
+    //  }
+    // document.getElementsByTagName('head')[0].appendChild(link)
+
+
+}
