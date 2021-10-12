44a27e3dda4f7933ab7be22ae4e2e0776c088e39
KonTagowski <konrad.tagowski97@gmail.com>
2021-10-12 14:42:17 +0200

    Changes: - Fix and new feature. Plan: - Menu to choose a color palette.

--- js/script.js
+++ js/script.js
@@ -6,16 +6,8 @@ function changeStyle(href, id_StyleSheet) {
     link.type = 'text/css';
     link.href = "css/" + href;
     document.getElementsByTagName('head')[0].replaceChild(link, document.styleSheets[id_StyleSheet].ownerNode);
-
-    //  for (var i = 0; i < document.styleSheets.length; i++) {
-    //      if (document.styleSheets[i].title == id_title) {
-    //          //document.getElementsByTagName('head')[0].removeChild(document.styleSheets[i].ownerNode);
-    //          document.getElementsByTagName('head')[0].replaceChild(link, document.styleSheets[i].ownerNode);
-    //      }
-    //  }
-    // document.getElementsByTagName('head')[0].appendChild(link)
-
 openMenu();
 closeMenu();
 openMenu();
 }
+
